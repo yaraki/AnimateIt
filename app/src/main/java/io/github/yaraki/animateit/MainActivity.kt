acd9c725d8ac63acf33fab525d4fb855dbbd88ce
Yuichi Araki <yaraki@google.com>
2020-01-06 17:01:45 +0900

    Slide scaling

--- app/src/main/java/io/github/yaraki/animateit/MainActivity.kt
+++ app/src/main/java/io/github/yaraki/animateit/MainActivity.kt
@@ -1,11 +1,10 @@
 package io.github.yaraki.animateit
 
-import android.os.Build
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import io.github.yaraki.animateit.ui.DisplayInfoDialogFragment
 
 private const val TAG_DISPLAY_INFO = "display_info"
@@ -16,26 +15,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        val displayInfo: Button = findViewById(R.id.display_info)
-        displayInfo.setOnClickListener {
+        findViewById<Button>(R.id.display_info).setOnClickListener {
             DisplayInfoDialogFragment().show(supportFragmentManager, TAG_DISPLAY_INFO)
         }
-    }
 
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
-            hideSystemUi()
+        findViewById<FloatingActionButton>(R.id.start).setOnClickListener {
+            startActivity(Intent(this, SlideActivity::class.java))
         }
-    }
-
-    private fun hideSystemUi() {
-        @RequiresApi(Build.VERSION_CODES.KITKAT)
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
