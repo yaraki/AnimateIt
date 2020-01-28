7777c9859c696b409407db060db2e7bf5ea46b0a
Yuichi Araki <yaraki@google.com>
2020-01-28 17:36:05 +0900

    ObjectAnimator

--- app/src/main/java/io/github/yaraki/animateit/deck/s02api/ObjectAnimatorFragment.kt
+++ app/src/main/java/io/github/yaraki/animateit/deck/s02api/ObjectAnimatorFragment.kt
@@ -16,26 +16,55 @@
 
 package io.github.yaraki.animateit.deck.s02api
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import io.github.yaraki.animateit.R
+import android.view.animation.LinearInterpolator
+import io.github.yaraki.animateit.databinding.PageObjectAnimatorBinding
 import io.github.yaraki.animateit.deck.Page
 import io.github.yaraki.animateit.deck.PageFragment
 
 class ObjectAnimatorFragment : PageFragment() {
 
     companion object : Page {
-        override fun create() =
-            ObjectAnimatorFragment()
+        override fun create() = ObjectAnimatorFragment()
     }
+
+    private lateinit var binding: PageObjectAnimatorBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.page_object_animator, container, false)
+        binding = PageObjectAnimatorBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ObjectAnimator.ofFloat(binding.demoRotation, View.ROTATION, 0f, 360f).apply {
+            interpolator = LinearInterpolator()
+            duration = 3000
+            repeatCount = ObjectAnimator.INFINITE
+        }.start()
+        ObjectAnimator.ofInt(binding.demoSeek, "progress", 0, 100).apply {
+            interpolator = LinearInterpolator()
+            duration = 3000
+            repeatCount = ObjectAnimator.INFINITE
+        }.start()
+        ObjectAnimator.ofPropertyValuesHolder(
+            binding.demoMultiple,
+            PropertyValuesHolder.ofFloat(View.SCALE_X, 0.6f, 1.2f, 0.6f),
+            PropertyValuesHolder.ofFloat(View.SCALE_Y, 1.2f, 0.6f, 1.2f)
+        ).apply {
+            interpolator = LinearInterpolator()
+            duration = 3000
+            repeatCount = ObjectAnimator.INFINITE
+        }.start()
     }
 }
